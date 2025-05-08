12a870c5ad13f87ff35544fa19b79bf2925f5275
Milosz Zubala <miloszzubala@gmail.com>
2025-05-08 19:45:41 +0200

    a

--- html_parser/parser.py
+++ html_parser/parser.py
@@ -13,7 +13,55 @@ from common.utils import read_file, get_offer_id_from_url
 from db.models import Offer
 from common.fast_process import fast_multi_process
 
-from typing import Mapping, TypeVar
+from typing import Dict, List, Mapping, Optional, TypeVar, TypedDict, Union, cast
+
+import msgspec
+
+JsonType = Union[None, int, str, bool, List['JsonType'], Dict[str, 'JsonType']]
+
+class Coordinates(msgspec.Struct):
+    latitude: Optional[float]
+    longitude: Optional[float]
+
+
+class Address(msgspec.Struct):
+    province: Optional[str]
+    city: Optional[str]
+    subregion: Optional[str]
+    district: Optional[str]
+    street: Optional[str]
+
+
+class Characteristics(msgspec.Struct):
+    price: Optional[int] = 0
+    size: Optional[int] = 0
+    rooms: Optional[int] = None
+    market_type: Optional[str] = None
+    building_type: Optional[str] = None
+    floor_number: Optional[int] = None
+    building_floors_num: Optional[int] = None
+    material: Optional[str] = None
+    window_type: Optional[str] = None
+    heating: Optional[str] = ""
+    year_built: Optional[int] = None
+    rent: Optional[int] = None
+    ownership: Optional[str] = None
+    construction_status: Optional[str] = None
+
+
+class Ad(msgspec.Struct):
+    id: int
+    title: str
+    features: str
+    description: str
+    url: Optional[str]
+
+    coordinates: Coordinates
+    address: Address
+    offer_type: Optional[str]
+
+    characteristics: Characteristics
+    
 
 R = TypeVar("R")
 
@@ -36,31 +84,53 @@ def get_safe_nested(
         return value
     return default
 
-def extract_json_strings(text: str) -> list[str]:
-    json_strings = []
-    stack = []
-    start_indices = []
+def extract_json_with_key(text: str, key: str) -> JsonType | None:
 
-    for i, char in enumerate(text):
-        if char == '{':
-            if not stack:
-                start_indices.append(i)
-            stack.append('{')
-        elif char == '}':
-            if stack:
-                stack.pop()
-                if not stack:
-                    start = start_indices.pop(0)
-                    candidate = text[start:i+1]
-                    # Validate if it's a valid JSON object
-                    try:
-                        json.loads(candidate)
-                        json_strings.append(candidate)
-                    except json.JSONDecodeError:
-                        pass  # Not a valid JSON, skip
+    count = 0
+    # initial key search
+    start_index = text.find(key)
+    if start_index == -1:
+        return None
+    
+    # Now find the first '{' after the key
+    brace_start = text.find("{", start_index)
+    if brace_start == -1:
+        return None
+    
+    brace_end = brace_start
+    # count curly braces
+    for i, char in enumerate(text[brace_start:], start=brace_start):
+        if char == "{":
+            count += 1
+        elif char == "}":
+            count -= 1
+        if count == 0:
+            brace_end = i
+            break
+    
+    # decode
+    if brace_end > start_index:
+        json_str = text[brace_start:brace_end + 1]
+        try:
+            json_obj = json.loads(json_str)
+            return json_obj
+        except json.JSONDecodeError:
+            logger.warning(f"Invalid JSON string: {json_str}")
+            return None
+    
+    return None
 
-    return json_strings
-
+def preprocess_json(json: JsonType) -> None:
+    if isinstance(json, dict):
+        characteristics_list = json.get("characteristics", None)
+        if isinstance(characteristics_list, list):
+            characteristics = {str(item["key"]): item['value'] for item in characteristics_list if isinstance(item, dict) and item.get("key") and item.get("value")}
+            json["characteristics"] = characteristics
+            
+        features_list = json.get("features", None)
+        if isinstance(features_list, list):
+            json["features"] = ",".join(str(f) for f in features_list)
+        
 
 async def parse_offer(content: DocContent) -> Offer | None:
     sel = Selector(text=content)
@@ -71,63 +141,59 @@ async def parse_offer(content: DocContent) -> Offer | None:
         logger.warning(f"Offer ID not found in {canonical}.")
         return None
 
-    json_strings = extract_json_strings(content)
-
-    json_data = None
-    for json_string in json_strings[:20]:  # sprawdzamy tylko pierwsze 20 dla wydajności
-        if '{"props":' in json_string:
-            try:
-                json_data = json.loads(json_string)
-            except json.JSONDecodeError:
-                logger.warning(f"Failed to decode JSON string: {json_string}")
-            break
-    if json_data is None:
-        logger.warning(f"JSON data not found in {canonical}.")
+    key_to_search = '"ad":{'
+    json_raw = extract_json_with_key(content, key_to_search)
+    
+    if json_raw is None:
+        logger.warning(f"JSON object with tag `{key_to_search}` not found in {canonical}.")
         return None
+
+    preprocess_json(json_raw)
+    ad = msgspec.convert(json_raw, type=Ad)
+        
     
-    ad = json_data["props"]["pageProps"]["ad"]
-    location = ad.get("location", {})
-    address = location.get("address", {})
-    coordinates = location.get("coordinates", {})
     
-    characteristics_list = ad.get("characteristics", [])
-    characteristics = {item["key"]: item for item in characteristics_list if "key" in item and "value" in item}
+    # characteristics_list = ad.get("characteristics", [])
+    # characteristics = {item["key"]: item for item in characteristics_list if "key" in item and "value" in item}
 
-    offer = Offer(
-        id=get_safe_nested(ad, ["id"], ""),
-        title=get_safe_nested(ad, ["title"], ""),
-        features=",".join(get_safe_nested(ad, ["features"], [])),
-        description=html.unescape(get_safe_nested(ad, ["description"], "")),
-        url=get_safe_nested(ad, ["url"], None),
-        latitude=get_safe_nested(coordinates, ["latitude"], None),
-        longitude=get_safe_nested(coordinates, ["longitude"], None),
-        province=get_safe_nested(address, ["province", "name"], None),
-        city=get_safe_nested(address, ["city", "name"], None),
-        subregion=get_safe_nested(address, ["county", "name"], None),
-        district=get_safe_nested(address, ["district", "name"], None),
-        street=get_safe_nested(address, ["street", "name"], None),
+    
+    
+    logger.info(f"Parsed ad: {ad}")
+    # offer = Offer(
+    #     id=get_safe_nested(ad, ["id"], ""),
+    #     title=get_safe_nested(ad, ["title"], ""),
+    #     features=",".join(get_safe_nested(ad, ["features"], [])),
+    #     description=html.unescape(get_safe_nested(ad, ["description"], "")),
+    #     url=get_safe_nested(ad, ["url"], None),
+    #     latitude=get_safe_nested(coordinates, ["latitude"], None),
+    #     longitude=get_safe_nested(coordinates, ["longitude"], None),
+    #     province=get_safe_nested(address, ["province", "name"], None),
+    #     city=get_safe_nested(address, ["city", "name"], None),
+    #     subregion=get_safe_nested(address, ["county", "name"], None),
+    #     district=get_safe_nested(address, ["district", "name"], None),
+    #     street=get_safe_nested(address, ["street", "name"], None),
 
-        offer_type=get_safe_nested(ad, ["adCategory", "type"], None),
+    #     offer_type=get_safe_nested(ad, ["adCategory", "type"], None),
 
-        price = get_safe_nested(characteristics, ["price", "value"], 0),
-        size = get_safe_nested(characteristics, ["m", "value"], 0),
-        rooms = get_safe_nested(characteristics, ["rooms_num", "value"], None),
-        market_type=get_safe_nested(characteristics, ["market", "value"], None),
-        building_type=get_safe_nested(characteristics, ["building_type", "value"], None),
-        floor_number=get_safe_nested(characteristics, ["floor_no", "value"], None),
-        building_floors_num=get_safe_nested(characteristics, ["building_floors_num", "value"], None),
-        material=get_safe_nested(characteristics, ["building_material", "value"], None),
-        window_type=get_safe_nested(characteristics, ["windows_type", "value"], None),
-        heating=get_safe_nested(characteristics, ["heating", "value"], ""),
-        year_built=get_safe_nested(characteristics, ["build_year", "value"], None),
-        rent=get_safe_nested(characteristics, ["rent", "value"], None),
-        ownership=get_safe_nested(characteristics, ["building_ownership", "value"], None),
-        construction_status=get_safe_nested(characteristics, ["construction_status", "value"], None),
-    )
+    #     price = get_safe_nested(characteristics, ["price", "value"], 0),
+    #     size = get_safe_nested(characteristics, ["m", "value"], 0),
+    #     rooms = get_safe_nested(characteristics, ["rooms_num", "value"], None),
+    #     market_type=get_safe_nested(characteristics, ["market", "value"], None),
+    #     building_type=get_safe_nested(characteristics, ["building_type", "value"], None),
+    #     floor_number=get_safe_nested(characteristics, ["floor_no", "value"], None),
+    #     building_floors_num=get_safe_nested(characteristics, ["building_floors_num", "value"], None),
+    #     material=get_safe_nested(characteristics, ["building_material", "value"], None),
+    #     window_type=get_safe_nested(characteristics, ["windows_type", "value"], None),
+    #     heating=get_safe_nested(characteristics, ["heating", "value"], ""),
+    #     year_built=get_safe_nested(characteristics, ["build_year", "value"], None),
+    #     rent=get_safe_nested(characteristics, ["rent", "value"], None),
+    #     ownership=get_safe_nested(characteristics, ["building_ownership", "value"], None),
+    #     construction_status=get_safe_nested(characteristics, ["construction_status", "value"], None),
+    # )
 
     
     # logger.info(f"Json data: {offer}")
-    return offer
+    # return offer
 
 
 async def parse_file(file_path: Path) -> None:
